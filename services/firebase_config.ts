ab575ea95f6492489a8aaf2f7760f7b87689587f
Joshua4221 <joshuaejike4221@gmail.com>
2025-02-13 00:54:36 +0100

    just

--- services/firebase_config.ts
+++ services/firebase_config.ts
@@ -6,15 +6,6 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyAdhGGbzYuJeu7AL5tsy0lfBaJZbT2wquU',
-  authDomain: 'test-app-cbefd.firebaseapp.com',
-  projectId: 'test-app-cbefd',
-  storageBucket: 'test-app-cbefd.firebasestorage.app',
-  messagingSenderId: '839650303120',
-  appId: '1:839650303120:web:0ea830bf94b979e6d68c15',
-  measurementId: 'G-ZB25M0V1ZN',
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- utils/video_utils.ts
+++ utils/video_utils.ts
@@ -1,46 +1,23 @@
-export const playVideo = async (
-  index: number,
-  videoRefs: any,
-  setIsPlaying: (x: boolean) => void
-) => {
-  const video = videoRefs.current[index];
-  if (video) {
-    await video.playAsync();
-    setIsPlaying(true);
-  }
-};
+// export const playVideo = async (index: number, videoRefs) => {
+//   const video = videoRefs.current[index];
+//   if (video) {
+//     await video.playAsync();
+//     setIsPlaying(true);
+//   }
+// };
 
-export const pauseVideo = async (
-  index: number,
-  videoRefs: any,
-  setIsPlaying: (x: boolean) => void
-) => {
-  const video = videoRefs.current[index];
-  if (video) {
-    await video.pauseAsync();
-    setIsPlaying(false);
-  }
-};
+// export const pauseVideo = async (index: number, videoRefs) => {
+//   const video = videoRefs.current[index];
+//   if (video) {
+//     await video.pauseAsync();
+//     setIsPlaying(false);
+//   }
+// };
 
-export const startVideo = async (
-  index: number,
-  videoRefs: any,
-  setIsPlaying: (x: boolean) => void
-) => {
-  const video = videoRefs.current[index];
-  if (video) {
-    await video.replayAsync();
-    setIsPlaying(false);
-  }
-};
-
-export const handlePlayPress = async (
-  video: any,
-  setCurrentVideo: any,
-  setPlaybackPosition: any,
-  router: any
-) => {
-  setCurrentVideo(video);
-  setPlaybackPosition(0);
-  router.push('/(tabs)/short');
-};
+// export const startVideo = async (index: number, videoRefs) => {
+//   const video = videoRefs.current[index];
+//   if (video) {
+//     await video.replayAsync();
+//     setIsPlaying(false);
+//   }
+// };
